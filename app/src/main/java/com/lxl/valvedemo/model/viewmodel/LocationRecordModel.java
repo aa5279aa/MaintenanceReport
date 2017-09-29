89bf03213db7a5d49e515919c0be48bc9ac9e120
xiangleiliu <xiangleiliu@Ctrip.com>
2017-09-29 13:22:54 +0800

    百度定位SDK

--- app/src/main/java/com/lxl/valvedemo/model/viewmodel/LocationRecordModel.java
+++ app/src/main/java/com/lxl/valvedemo/model/viewmodel/LocationRecordModel.java
@@ -4,4 +4,12 @@ package com.lxl.valvedemo.model.viewmodel;
  * Created by xiangleiliu on 2017/9/29.
  */
 public class LocationRecordModel {
+
+    public double longitude = 5.56;
+    public double latitude = 5.56;
+    public String locationName = "";//位置名称
+
+    public int x;
+    public int y;
+
 }
