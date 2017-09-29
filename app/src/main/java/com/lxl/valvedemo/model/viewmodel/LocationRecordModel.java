e233ef5340a83bd7f3b7210bbff811f0eca10834
xiangleiliu <xiangleiliu@Ctrip.com>
2017-09-29 13:21:51 +0800

    百度定位SDK

---
+++ app/src/main/java/com/lxl/valvedemo/model/viewmodel/LocationRecordModel.java
@@ -0,0 +1,7 @@
+package com.lxl.valvedemo.model.viewmodel;
+
+/**
+ * Created by xiangleiliu on 2017/9/29.
+ */
+public class LocationRecordModel {
+}
---
+++ app/src/main/java/com/lxl/valvedemo/view/LocationRecordShowView.java
@@ -0,0 +1,7 @@
+package com.lxl.valvedemo.view;
+
+/**
+ * Created by xiangleiliu on 2017/9/29.
+ */
+public class LocationRecordShowView {
+}
